3102388d710125bf236038a662f33f20793ed4a0
rnat697 <rnat697@aucklanduni.ac.nz>
2023-04-23 00:14:18 +1200

    Added top left corner bubble and header layout

--- snakes-and-ladders/src/components/AvatarPage.jsx
+++ snakes-and-ladders/src/components/AvatarPage.jsx
@@ -1,25 +1,46 @@
 import '../css/AvatarPage.css'
 import { Link } from "react-router-dom"
+import bubblecornerTop from "../assets/bubble_top_left.png";
 
 function AvatarPage() {
   return (
-    <div>
-      <div>
-        This is where you select your avatar
+    <body>
+      <div className='avatar-container'>
+        <div className='avatar-heading'>
+          <div className='container-top-corner'>
+              <img className='bubble-corner-top' src={bubblecornerTop} />
+              <button className='back-btn'>{"<"}</button>
+          </div>
+          <div className='heading-text'>
+            <h1 className='heading-title'>
+              SELECT AVATAR
+            </h1>
+            <h2 className='heading-subtitle'>
+              PLAYER 1
+            </h2>
+            <input type='text' placeholder='Enter a nickname...' className='nickname-input'/>
+          </div>
+        </div>
+
+        <div className='avatar-content'>
+          <p>insert avatars here</p>
+        </div>
+
+        <div className='avatar-footer'>
+          <Link to="/avatar">
+              <button>
+                SELECT NEXT AVATAR
+              </button>
+            </Link>
+
+            <Link to="/game">
+              <button>
+                PLAY GAME
+              </button>
+            </Link>
+        </div>
       </div>
-
-      <Link to="/avatar">
-        <button>
-          SELECT NEXT AVATAR
-        </button>
-      </Link>
-
-      <Link to="/game">
-        <button>
-          PLAY GAME
-        </button>
-      </Link>
-    </div>
+    </body>
   )
 }
 
