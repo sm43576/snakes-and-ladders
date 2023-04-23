b71b32a885c40d583a0258b34f5f290b3a568deb
rnat697 <rnat697@aucklanduni.ac.nz>
2023-04-23 22:25:22 +1200

    Added avatar circles and linked back button to players route

--- snakes-and-ladders/src/components/AvatarPage.jsx
+++ snakes-and-ladders/src/components/AvatarPage.jsx
@@ -2,26 +2,31 @@ import '../css/AvatarPage.css'
 import { Link } from "react-router-dom"
 import bubblecornerTop from "../assets/bubble_top_left.png";
 
+const avatarImageFiles = ["avatar_pufferfish.png", "avatar_dolphin.png", "avatar_otter.png", "avatar_squid.png",
+                          "avatar_seal.png", "avatar_octopus.png", "avatar_shark.png", "avatar_tropic_fish.png"];
+
 function AvatarPage() {
   return (
     <body>
       <div className='avatar-container'>
+
         <div className='avatar-heading'>
           <div className='container-top-corner'>
               <img className='bubble-corner-top' src={bubblecornerTop} />
-              <button className='back-btn'>{"<"}</button>
+              <Link to="/players"><button className='back-btn'>{"<"}</button></Link>
           </div>
-          <h1 className='heading-title'>
-            SELECT AVATAR
-          </h1>
-          <h2 className='heading-subtitle'>
-            PLAYER 1
-          </h2>
+          <h1 className='heading-title'>SELECT AVATAR</h1>
+          <h2 className='heading-subtitle'>PLAYER 1</h2>
           <input className='nickname-input' type='text' placeholder='Enter a nickname...' />
         </div>
 
         <div className='avatar-content'>
-          <p>insert avatars here</p>
+          {avatarImageFiles.map(file=>(
+            <div className='avatar-circles'>
+              {/* TODO ADD BUTTONS */}
+              <img className='avatar-images' key={file} src={`../src/assets/selectable_avatars/${file}`}/>
+            </div>
+          ))}
         </div>
 
         <div className='avatar-footer'>
