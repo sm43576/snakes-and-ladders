371e9683e27ec50bafe67e7e44effdc38c986e97
jjun273 <jjun273@aucklanduni.ac.nz>
2023-04-27 00:57:08 +1200

    Created background of results page

--- snakes-and-ladders/src/components/ResultsPage.jsx
+++ snakes-and-ladders/src/components/ResultsPage.jsx
@@ -1,15 +1,22 @@
-import "../css/SettingsPopUp.css"
+import "../css/ResultsPage.css"
 import { Link } from "react-router-dom"
 
 function ResultsPage() {
+
   return (
-    <div>
-      <Link to="/">
-        <button>
-          BACK TO HOME
-        </button>
-      </Link>
-    </div>
+    <div className="game-page">
+      <div>
+        <Link to="/">
+          <button>
+            BACK TO HOME
+          </button>
+        </Link>
+      </div>
+      <h1>Results</h1>
+      <div class="rectangle"></div>
+
+
+    </div>    
   )
 }
 
