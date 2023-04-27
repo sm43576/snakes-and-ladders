ff285979b7702e8c23c8f2be519ac4b63eb0f211
ykim273 <ykim273@aucklanduni.ac.nz>
2023-04-27 15:42:10 +1200

    Add needed components in settings screen

--- snakes-and-ladders/src/components/SettingsPopUp.jsx
+++ snakes-and-ladders/src/components/SettingsPopUp.jsx
@@ -8,7 +8,30 @@ function SettingsPopUp(props) {
         <button className="close-btn" onClick={() => props.setTrigger(false)}>
           close
         </button>
-        <h1>Settings hello hello</h1>
+        <h1>SETTINGS</h1>
+        <h2>PAUSED</h2>
+        <div className="div-adjustments">
+          <div className="overall-volume">
+            <p>Overall Volume</p>
+          </div>
+          <div className="sfx-volume">
+            <p>SFX Volume</p>
+          </div>
+          <div className="music-volume">
+            <p>Music Volume</p>
+          </div>
+          <div className="play-speed">
+            <p>Play Speed</p>
+            <button className="1-times">1 x</button>
+            <button className="1.5-times">1.5 x</button>
+            <button className="2-times">2 x</button>
+          </div>
+        </div>
+        <div className="div-exitbuttons">
+          <button className="resume">RESUME</button>
+          <button className="restart">RESTART</button>
+          <button className="quit">QUIT</button>
+        </div>
         {props.children}
       </div>
     </div>
