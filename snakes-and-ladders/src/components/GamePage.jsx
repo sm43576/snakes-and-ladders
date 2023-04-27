77fd8e298acf7d5220bbef35f54b258c74f04b85
ykim273 <ykim273@aucklanduni.ac.nz>
2023-04-27 16:24:16 +1200

    Implement tutorial pop up from game page

--- snakes-and-ladders/src/components/GamePage.jsx
+++ snakes-and-ladders/src/components/GamePage.jsx
@@ -1,36 +1,33 @@
-import "../css/GamePage.css"
-import { Link } from "react-router-dom"
+import "../css/GamePage.css";
+import { Link } from "react-router-dom";
+import TutorialPopUp from "./TutorialPopUp";
+import { useState } from "react";
 
 function GamePage() {
+  const [tutorialButtonPopup, setTutorialButtonPopup] = useState(false);
   return (
     <div>
-      <div>
-        This is the main game
-      </div>
+      <div>This is the main game</div>
 
       <Link to="/">
-        <button>
-          BACK TO HOME
-        </button>
+        <button>BACK TO HOME</button>
       </Link>
 
       {/* POPOVER NOT IMPLEMENTED */}
-      <button>
-        SETTINGS
-      </button>
+      <button>SETTINGS</button>
 
       {/* POPOVER NOT IMPLEMENTED */}
-      <button>
-        HOW TO PLAY
-      </button>
+      <button onClick={() => setTutorialButtonPopup(true)}>HOW TO PLAY</button>
+      <TutorialPopUp
+        trigger={tutorialButtonPopup}
+        setTrigger={setTutorialButtonPopup}
+      ></TutorialPopUp>
 
       <Link to="/results">
-        <button>
-          RESULTS
-        </button>
+        <button>RESULTS</button>
       </Link>
     </div>
-  )
+  );
 }
 
-export default GamePage
+export default GamePage;
