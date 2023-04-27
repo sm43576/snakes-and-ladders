eb6a6526137f9e7f1d7b5af8e986f8910afec65a
ykim273 <ykim273@aucklanduni.ac.nz>
2023-04-27 15:13:38 +1200

    Implement settings pop up from game page

--- snakes-and-ladders/src/components/GamePage.jsx
+++ snakes-and-ladders/src/components/GamePage.jsx
@@ -1,36 +1,33 @@
-import "../css/GamePage.css"
-import { Link } from "react-router-dom"
+import "../css/GamePage.css";
+import { Link } from "react-router-dom";
+import SettingsPopUp from "./SettingsPopUp";
+import { useState } from "react";
 
 function GamePage() {
+  const [settingsButtonPopup, setSettingsButtonPopup] = useState(false);
   return (
     <div>
-      <div>
-        This is the main game
-      </div>
+      <div>This is the main game</div>
 
       <Link to="/">
-        <button>
-          BACK TO HOME
-        </button>
+        <button>BACK TO HOME</button>
       </Link>
 
       {/* POPOVER NOT IMPLEMENTED */}
-      <button>
-        SETTINGS
-      </button>
+      <button onClick={() => setSettingsButtonPopup(true)}>SETTINGS</button>
+      <SettingsPopUp
+        trigger={settingsButtonPopup}
+        setTrigger={setSettingsButtonPopup}
+      ></SettingsPopUp>
 
       {/* POPOVER NOT IMPLEMENTED */}
-      <button>
-        HOW TO PLAY
-      </button>
+      <button>HOW TO PLAY</button>
 
       <Link to="/results">
-        <button>
-          RESULTS
-        </button>
+        <button>RESULTS</button>
       </Link>
     </div>
-  )
+  );
 }
 
-export default GamePage
+export default GamePage;
