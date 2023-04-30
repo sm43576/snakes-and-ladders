d52d4a8e0ac13c1173159b9a902c3d8956360896
rnat697 <rnat697@aucklanduni.ac.nz>
2023-04-30 16:21:26 +1200

    Added persistent selection border colour on avatar button upon selection

--- snakes-and-ladders/src/components/AvatarPage.jsx
+++ snakes-and-ladders/src/components/AvatarPage.jsx
@@ -9,18 +9,19 @@ const avatarImageFiles = ["avatar_pufferfish.png", "avatar_dolphin.png", "avatar
                           "avatar_seal.png", "avatar_octopus.png", "avatar_shark.png", "avatar_tropic_fish.png"];
 
 function AvatarPage() {
-  const currentPlayer= { // TODO Temporary
+  const currentPlayer= { // TODO: Temporary
     name: 'Player 1', 
     id: 1,
     avatarFile: '',
   }
   const[players, setPlayer] = useState([currentPlayer]);
   const [gameBtnState, setGameBtnState] = useState(false);
-  // setPlayer(currentPlayer); // TODO set current player based on max number of players?
-
+  const[activeAvatar, setActiveAvatar] = useState("");
+  // setPlayer(currentPlayer); // TODO: set current player based on max number of players?
   
   // When player selects an avatar to use prob need to save it in a database or something
   function handleAvatarBtnClick( avatarFileName) {
+    setActiveAvatar(avatarFileName);
     const index = players.findIndex(function(c){
       return c.id== currentPlayer.id
     });
@@ -29,15 +30,14 @@ function AvatarPage() {
       ...currentPlayer,
       avatarFile: avatarFileName
     }
-    setPlayer(tempPlayersArray);
-    console.log(players);
-    // TODO LINK TO BACKEND DATABASE OR PASS IT THROUGH TO NEXT PAGE
+    setPlayer(tempPlayersArray); // only updates next render tho so maybe database instead?
+    // TODO: LINK TO BACKEND DATABASE OR PASS IT THROUGH TO NEXT PAGE
     canPlayersStartGame(true); // Checks if last player has chosen avatar in order to play game
   }
 
   // Enables "Start game" button if last player has chosen an avatar
   function canPlayersStartGame (hasChosen){
-    if(hasChosen && currentPlayer.id == 1){ // TODO 4 is temporary intend to use maxPlayer context from numPlayers page
+    if(hasChosen && currentPlayer.id == 1){ // TODO: 1 is temporary intend to use maxPlayer context from numPlayers page
       setGameBtnState(true);
     }else{
       setGameBtnState(false);
@@ -59,7 +59,7 @@ function AvatarPage() {
       <div className='avatar-content'>
         {avatarImageFiles.map(file=>(
           <div className='avatar-circles'>
-            <button className='avatar-img-btn' key={"button"+file} onClick={()=>handleAvatarBtnClick(file)}>
+            <button className= {activeAvatar == file ? 'selected-avatar-img-btn':'default-avatar-img-btn'} key={"button"+file} onClick={()=>handleAvatarBtnClick(file)}>
               <img className='avatar-images' key={file} src={`../src/assets/selectable_avatars/${file}`}/>
             </button>
           </div>
@@ -68,8 +68,8 @@ function AvatarPage() {
       
       {/* ------ Next button & star game button -----*/}
       <img className='bubble-corner-btm' src={bubbleCornerBtm} />
-      <Link to="/avatar"> {/*TODO redirect with ID numbers i.e. "/avatar/1", "/avatar/2" etc */}
-        <button className='next-btn' disabled={currentPlayer.id == 1}>{'>'}</button> {/*NOTE 1 is temporary, intend to use maxPlayers from previous page. Only disables when the last player is selecting*/}
+      <Link to="/avatar"> {/*TODO: redirect with ID numbers i.e. "/avatar/1", "/avatar/2" etc */}
+        <button className='next-btn' disabled={currentPlayer.id == 1}>{'>'}</button> {/*TODO: 1 is temporary, intend to use maxPlayers from previous page. Only disables when the last player is selecting*/}
       </Link>
       <Link to="/game">
         <button className='start-game-btn' disabled={!gameBtnState}>{'PLAY GAME'}</button>
