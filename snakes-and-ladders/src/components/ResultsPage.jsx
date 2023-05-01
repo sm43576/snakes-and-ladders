19dd66757ad42880b808468005feb307d7a8db6f
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-01 16:30:28 +1200

    Added ranking text

--- snakes-and-ladders/src/components/ResultsPage.jsx
+++ snakes-and-ladders/src/components/ResultsPage.jsx
@@ -12,15 +12,21 @@ function ResultsPage() {
 
       <div class="div2">
         <div class="first">
-          <div class="rectangle1"></div>
+          <div class="rectangle1">
+            <p class="first-text">1st</p>
+          </div>
           <div class="avatar1"></div>
         </div>
         <div class="second">
-          <div class="rectangle2"></div>
+          <div class="rectangle2">
+            <p class="second-third-text">2nd</p>
+          </div>
           <div class="avatar2"></div>
         </div>
         <div class="third">
-          <div class="rectangle3"></div>
+          <div class="rectangle3">
+            <p class="second-third-text">3rd</p>
+          </div>
           <div class="avatar3"></div>
         </div>
         <div class="button-position">
