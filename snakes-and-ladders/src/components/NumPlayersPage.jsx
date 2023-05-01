30bb710c67fa0576beec3c7beac68670ccab05eb
sm43576 <79734318+sm43576@users.noreply.github.com>
2023-05-01 21:42:26 +1200

    Replaced assests with bubble+name

--- snakes-and-ladders/src/components/NumPlayersPage.jsx
+++ snakes-and-ladders/src/components/NumPlayersPage.jsx
@@ -6,20 +6,19 @@ function NumPlayersPage() {
   return (
     <div className="player-container">
       
-      {/* <img className="background" src ="../src/assets/player-background.png"></img> */}
+      
       <img className= "bubble-top" src="../src/assets/bubble_top_left.png"></img>
       <img className= "bubble-bot" src="../src/assets/bubble_btm_right.png"></img>
 
       <div>
         <div className = "players">
-            <img className="bubble-p" src="../src/assets/player-bubble.png"></img>
+            <img className="bubble-p" src="../src/assets/player_bubble.png"></img>
         </div>
         <div className = "computer-players">
-          <img className="bubble-c" src="../src/assets/player-bubble.png"></img>
+          <img className="bubble-c" src="../src/assets/com_bubble.png"></img>
         </div>
 
-        <NavLink to={"/avatar/0/"+ maxPlayers}> {/* TODO: replace 2 with max number of players. LEAVE 0 ALONE since that indicates first player ~Rachel */}
-          <button className='avatar-btn'>
+        <NavLink to={"/avatar/0/"+ maxPlayers}>  <button className='avatar-btn'>
              {'>'}
           </button>
         </NavLink>
@@ -30,11 +29,10 @@ function NumPlayersPage() {
           </button>
         </Link>
 
-        <p className="players-text">  PLAYERS</p>
+  
         <button className="pIncrease-btn">{'>'}</button>
         <button className="pDecrease-btn">{'<'}</button>
 
-        <p className="com-text">COM PLAYERS</p>
         <button className="cIncrease-btn">{'>'}</button>
         <button className="cDecrease-btn">{'<'}</button>
 
