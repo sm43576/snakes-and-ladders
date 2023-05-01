587328aedaa9dc6007b9a70975e765b754030448
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-01 16:23:13 +1200

    Added avatar icons

--- snakes-and-ladders/src/components/ResultsPage.jsx
+++ snakes-and-ladders/src/components/ResultsPage.jsx
@@ -11,9 +11,18 @@ function ResultsPage() {
       </div>
 
       <div class="div2">
-        <div class="rectangle1"></div>
-        <div class="rectangle2"></div>
-        <div class="rectangle3"></div>
+        <div class="first">
+          <div class="rectangle1"></div>
+          <div class="avatar1"></div>
+        </div>
+        <div class="second">
+          <div class="rectangle2"></div>
+          <div class="avatar2"></div>
+        </div>
+        <div class="third">
+          <div class="rectangle3"></div>
+          <div class="avatar3"></div>
+        </div>
         <div class="button-position">
           <Link to="/">
             <button class="home-button">
