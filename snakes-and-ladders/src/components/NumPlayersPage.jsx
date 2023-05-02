1aa3595b360878ce74b2eb4e9f88572a173ecf3e
sm43576 <79734318+sm43576@users.noreply.github.com>
2023-05-02 14:05:21 +1200

    PlayerNopg. change to relative

--- snakes-and-ladders/src/components/NumPlayersPage.jsx
+++ snakes-and-ladders/src/components/NumPlayersPage.jsx
@@ -1,32 +1,47 @@
 import "../css/NumPlayersPage.css"
 import { NavLink, Link } from "react-router-dom"
+import { useState } from 'react';
 
 function NumPlayersPage() {
-  const maxPlayers = 1; // TODO: change this yourself lmao  ~Rachel
+
+  const [maxPlayers,changePlayer] = useState(0);
+  const [maxCommies,changeCom] = useState(0);
+
+  function handlePlayerCount(string){
+    if (string == '>')
+      changePlayer(maxPlayers+1);
+
+    else if((string =='<') &&(maxPlayers!=0))
+      changePlayer(maxPlayers-1);
+    console.log(maxPlayers);
+  }
+
+  function handleComCount(string){
+    if (string == '>')
+      changeCom(maxCommies+1);
+
+    else if((string =='<') &&(maxCommies!=0))
+      changeCom(maxCommies-1);
+    console.log(maxCommies);
+  }
+
+
+  
   return (
     <div className="player-container">
       
-
-      
-      <div className = "players">
-          <img className="bubble-p" src="../src/assets/player_bubble.png"></img>
-      </div>
-      <div className = "computer-players">
-        <img className="bubble-c" src="../src/assets/com_bubble.png"></img>
-      </div>
-
       <img className= "bubble-top" src="../src/assets/bubble_top_left.png"></img>
       <img className= "bubble-bot" src="../src/assets/bubble_btm_right.png"></img>
 
 
-      <button className="pIncrease-btn">{'>'}</button>
-      <button className="pDecrease-btn">{'<'}</button>
+      <button className="pIncrease-btn" onClick={()=>handlePlayerCount('>')}>{'>'}</button>
+      <button className="pDecrease-btn" onClick={()=>handlePlayerCount('<')}>{'<'}</button>
 
-      <button className="cIncrease-btn">{'>'}</button>
-      <button className="cDecrease-btn">{'<'}</button>
+      <button className="cIncrease-btn" onClick={()=>handleComCount('>')}>{'>'}</button>
+      <button className="cDecrease-btn" onClick={()=>handleComCount('<')}>{'<'}</button>
 
-      <p className="player-count">2</p>
-      <p className="com-count">3</p>
+      <p className="player-count">{maxPlayers}</p>
+      <p className="com-count">{maxCommies}</p>
 
         
         <Link to="/" >
@@ -39,6 +54,14 @@ function NumPlayersPage() {
              {'>'}
           </button>
         </NavLink>
+
+              
+      <div className = "players">
+          <img className="bubble-p" src="../src/assets/player_bubble.png"></img>
+      </div>
+      <div className = "computer-players">
+        <img className="bubble-c" src="../src/assets/com_bubble.png"></img>
+      </div>
     </div>
   )
 
