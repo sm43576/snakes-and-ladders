1f41dff83c997e567fc127bf14803f4c6d567613
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-05-10 17:58:00 +1200

    edit numPlayersPage css

--- frontend/src/pages/NumPlayersPage.jsx
+++ frontend/src/pages/NumPlayersPage.jsx
@@ -16,22 +16,21 @@ function NumPlayersPage() {
     addPlayer,
   } = useContext(AppContext);
 
-  // TODO: remove if we think using app context is a better idea
-  // const [maxPlayers,changePlayer] = useState(0);
-  // const [maxCommies,changeCom] = useState(0);
-
+  // Increases/decrease number of players accordingly - ensures total number of players (including COM) is max 6
   function handlePlayerCount(string) {
-    if (string == ">" && maxPlayers != 6) setMaxPlayers(maxPlayers + 1);
+    if (string == ">" && maxPlayers + maxCommies != 6) setMaxPlayers(maxPlayers + 1);
     else if (string == "<" && maxPlayers != 1) setMaxPlayers(maxPlayers - 1);
     console.log(maxPlayers);
   }
 
+  // Increases/decrease number of COM players accordingly - ensures total number of players (including human players) is max 6
   function handleComCount(string) {
-    if (string == ">" && maxCommies != 5) changeCom(maxCommies + 1);
+    if (string == ">" && maxPlayers + maxCommies != 6) changeCom(maxCommies + 1);
     else if (string == "<" && maxCommies != 0) changeCom(maxCommies - 1);
     console.log(maxCommies);
   }
 
+  // Adds players (including COM) to database with default name, placement and image
   async function handleAddPlayer() {
     for (let i = 0; i < maxPlayers; i++) {
       const newPlayer = await addPlayer(`Player ${i + 1}`, 0, "", true);
@@ -45,50 +44,55 @@ function NumPlayersPage() {
 
   return (
     <div className="num-players-page">
+      {/* Back Button */}
       <img className="bubble-top" src={bubbleCornerTop} />
       <Link to="/">
-        <button className="home-btn">{"<"}</button>
+        <button className="back-btn">{"<"}</button>
       </Link>
 
-      <div className="bubble-p">
-        <div className="player-type">
-          <p> PLAYERS </p>
+      <div className="bubbles-div">
+        {/* Players Bubble */}
+        <div className="bubble-p">
+          <div className="player-type">
+            <p> PLAYERS </p>
+          </div>
+          <div className="bubble-flex">
+            <button
+              className="decrease-btn"
+              onClick={() => handlePlayerCount("<")}>
+              <img className="decrease-img" src="../src/assets/decrease.png" />
+            </button>
+            <p className="player-count">{maxPlayers}</p>
+            <button
+              className="increase-btn"
+              aria-label="increase-btn1"
+              onClick={() => handlePlayerCount(">")}>
+              <img className="increase-img" src="../src/assets/increase.png" />
+            </button>
+          </div>
         </div>
-        <div className="bubble-flex">
-          <button
-            className="decrease-btn"
-            onClick={() => handlePlayerCount("<")}>
-            <img className="decrease-img" src="../src/assets/decrease.png" />
-          </button>
-          <p className="player-count">{maxPlayers}</p>
-          <button
-            className="increase-btn"
-            aria-label="increase-btn1"
-            onClick={() => handlePlayerCount(">")}>
-            <img className="increase-img" src="../src/assets/increase.png" />
-          </button>
+        {/* Com Players Bubble */}
+        <div className="bubble-c">
+          <div className="player-type">
+            <p> COM PLAYERS </p>
+          </div>
+          <div className="bubble-flex">
+            <button className="decrease-btn" onClick={() => handleComCount("<")}>
+              <img className="decrease-img" src="../src/assets/decrease.png" />
+            </button>
+            <p className="player-count">{maxCommies}</p>
+            <button className="increase-btn" onClick={() => handleComCount(">")}>
+              <img className="increase-img" src="../src/assets/increase.png" />
+            </button>
+          </div>
         </div>
       </div>
 
-      <div className="bubble-c">
-        <div className="player-type">
-          <p> COM PLAYERS </p>
-        </div>
-        <div className="bubble-flex">
-          <button className="decrease-btn" onClick={() => handleComCount("<")}>
-            <img className="decrease-img" src="../src/assets/decrease.png" />
-          </button>
-          <p className="player-count">{maxCommies}</p>
-          <button className="increase-btn" onClick={() => handleComCount(">")}>
-            <img className="increase-img" src="../src/assets/increase.png" />
-          </button>
-        </div>
-      </div>
-
+      {/* Next Button */}
       <img className="bubble-bot" src={bubbleCornerBtm} />
       <NavLink to={"/avatar/" + currentID + "/" + maxPlayers}>
         <button
-          className="avatar-btn"
+          className="next-btn"
           disabled={maxPlayers + maxCommies < 2}
           onClick={() => {
             handleAddPlayer();
