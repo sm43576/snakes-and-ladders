2653ccc53c22a05478566bc9b84b65e0461b509e
sm43576 <79734318+sm43576@users.noreply.github.com>
2023-05-11 23:30:24 +1200

    wave animations

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -2,6 +2,8 @@ import "../css/HomePage.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../AppContextProvider";
+import Wave from 'react-wavify'
+
 
 function HomePage() {
 
@@ -9,6 +11,27 @@ function HomePage() {
     players,
     removePlayer,
   } = useContext(AppContext);
+
+ const waveoptions= {
+    height: 30,
+    amplitude: 30,
+    speed: 0.15,
+    points: 6
+  }
+
+  const waveoptions2= {
+    height: 30,
+    amplitude: 20,
+    speed: 0.2,
+    points: 6
+  }
+  const waveoptions3= {
+    height: 20,
+    amplitude: 20,
+    speed: 0.4,
+    points: 6
+  }
+
 
   // Removes all existing players from the database
   async function clearDatabase() {
@@ -28,6 +51,30 @@ function HomePage() {
           onClick={() => clearDatabase()}
         >PLAY</button>
       </Link>
+
+      <Wave 
+        className="layer1"
+        fill='#99A0C4'
+        paused={false}
+        options={waveoptions}/>
+
+      <Wave 
+        className="layer2"
+        fill='#F6E1A7'
+        paused={false}
+        options={waveoptions2}/>
+
+      <Wave 
+        className="layer3"
+        fill='#BBC3DB'
+        paused={false}
+        options={waveoptions3}/>
+
+      <Wave 
+        className="layer4"
+        fill='#99A0C4'
+        paused={false}
+        options={waveoptions2}/>
     </div>
   );
 }
