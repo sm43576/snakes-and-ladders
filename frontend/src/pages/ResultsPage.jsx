c445f7ac26e884593e9554dd2cc034f76f290a81
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-05-12 04:04:23 +1200

    edit css for results page

--- frontend/src/pages/ResultsPage.jsx
+++ frontend/src/pages/ResultsPage.jsx
@@ -1,42 +1,38 @@
 import "../css/ResultsPage.css"
 import { Link } from "react-router-dom"
+import bubblesImg from "../assets/bubbles.png"
+import comAvatar from "../assets/selectable_avatars/avatar_com.png"
+import seaweed from "../assets/results_seaweed.gif"
 
 function ResultsPage() {
 
   return (
     <div className="results-page">
-      <h1 className="heading">Results</h1>
+      <img className="bubbles-animation" src={bubblesImg}></img>
+      <div className="results-content">
 
-      <div className="div2">
-        <div className="first">
-          <div className="rectangle1">
-            <p className="first-text">1st</p>
-          </div>
-          <div className="avatar1"></div>
-        </div>
-        <div className="second">
-          <div className="rectangle2">
-            <p className="second-third-text">2nd</p>
-          </div>
-          <div className="avatar2"></div>
-        </div>
-        <div className="third">
-          <div className="rectangle3">
-            <p className="second-third-text">3rd</p>
-          </div>
-          <div className="avatar3"></div>
-        </div>
-        <div className="button-position">
+          
 
-          <Link to="/" >
-            <button className="home-button">{'>'}</button>
-          </Link>
+          <div className="winner-player-image-div">
+            <img className="winner-player-image" src = {comAvatar}>
+            </img>
+          </div>
+
+          <img className="seaweed" src={seaweed}/>
+
+        <div className="podium">
+          <h1 className="winner-tag">Winner</h1>
+          <h2 className="winner-player-tag">Player</h2>
 
         </div>
+        <div className="avatar1"></div>
+
       </div>
 
 
-
+      <Link to="/" >
+        <button className="next-btn"></button>
+      </Link>
     </div>
   )
 }
