355591a9e3cffae8b266adedc8a67a43b9fa990f
rnat697 <rnat697@aucklanduni.ac.nz>
2023-05-12 10:48:14 +1200

    Home page test fix since removePlayers() call was added on HomePage
    - Requires the use of AppContext.Provider

--- frontend/src/_tests_/HomePage.test.jsx
+++ frontend/src/_tests_/HomePage.test.jsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 import { render,screen,fireEvent,getByRole} from '@testing-library/react';
 import { MemoryRouter, Route, Routes, Router } from 'react-router-dom';
 import { AppContext } from '../AppContextProvider';
@@ -11,29 +11,60 @@ import NumPlayersPage from '../pages/NumPlayersPage';
 
 
 test('test HomePage render/',() => {
-    const { getByText, queryByText } = render(
-        <MemoryRouter initialEntries={['/']}>
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-        </Routes>
-        </MemoryRouter>
-      );
-    
-    // "checks text"
-    expect(getByText('Snakes and Ladders')).toBeInTheDocument();
-    expect(getByText('PLAY')).toBeInTheDocument();
-    expect(queryByText('Play')).not.toBeInTheDocument();
+  const   player = [{
+    name:"Player 1",
+    placement: 0,
+    image: "",
+    isHuman: true
+  }];
+  const initState = {
+      players: [player],
+      removePlayer: vi.fn()
+};
+  const { getByText, queryByText } = render(
+      // <MemoryRouter initialEntries={['/']}>
+      // <Routes>
+      //   <Route path="/" element={<HomePage/>} />
+      // </Routes>
+      // </MemoryRouter>
+      // needs to use initState since removePlayer() is called in this page
+      <MemoryRouter initialEntries={['/']}>
+        <AppContext.Provider value={initState}>
+          <HomePage/>
+        </AppContext.Provider>
+      </MemoryRouter>
+    );
+  
+  // "checks text"
+  expect(getByText('Seaweed and Bubbles')).toBeInTheDocument();
+  expect(getByText('PLAY')).toBeInTheDocument();
+  expect(queryByText('Play')).not.toBeInTheDocument();
 })
 
 test('clicking button navigates to the specified route', async () => {
+    const   player = [{
+      name:"Player 1",
+      placement: 0,
+      image: "",
+      isHuman: true
+    }];
+    const initState = {
+        players: [player],
+        removePlayer: vi.fn()
+  };
     
     const { getByText, queryByText } = render(
+        // <MemoryRouter initialEntries={['/']}>
+        //   <Routes>
+        //     <Route path="/" element={<HomePage/>} />
+        //     <Route path="/players" element={<NumPlayersPage/>} />
+        //   </Routes>
+        // </MemoryRouter>
         <MemoryRouter initialEntries={['/']}>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/players" element={<NumPlayersPage/>} />
-          </Routes>
-        </MemoryRouter>
+        <AppContext.Provider value={initState}>
+          <HomePage/>
+        </AppContext.Provider>
+      </MemoryRouter>
       );
   
     // Simulate button click
@@ -43,6 +74,9 @@ test('clicking button navigates to the specified route', async () => {
 
     const { getByText: getByTextOnNextPage } = render(
       <MemoryRouter initialEntries={['/players']}>
+        <Routes>
+          <Route path="/players" element={<NumPlayersPage/>} />
+        </Routes>
     </MemoryRouter>
     );
   
