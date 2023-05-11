c6f504c3d84b571166e3918da0359f049cd7696e
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-12 05:39:29 +1200

    Added sound effects when opening results page

--- frontend/src/pages/ResultsPage.jsx
+++ frontend/src/pages/ResultsPage.jsx
@@ -1,11 +1,17 @@
 import "../css/ResultsPage.css"
 import { Link } from "react-router-dom"
 
+import seaweedSound from "../music/whistle-down.mp3";
+import bubbleSound from "../music/soap-bubbles-pop.mp3";
+import resultSound from "../music/tadaa.mp3";
+
 function ResultsPage() {
 
   return (
     <div className="results-page">
       <h1 className="heading">Results</h1>
+      <audio id="player" src={resultSound} autoPlay></audio>
+
 
       <div className="div2">
         <div className="first">
