6f614e25fb0ffb724e056bef7d48b718da20f89a
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-05-12 04:08:17 +1200

    removed unused images

--- frontend/src/pages/ResultsPage.jsx
+++ frontend/src/pages/ResultsPage.jsx
@@ -1,6 +1,6 @@
 import "../css/ResultsPage.css"
 import { Link } from "react-router-dom"
-import bubblesImg from "../assets/bubbles.png"
+import bubblesBackground from "../assets/bubbles.png"
 import comAvatar from "../assets/selectable_avatars/avatar_com.png"
 import seaweed from "../assets/results_seaweed.gif"
 
@@ -8,27 +8,21 @@ function ResultsPage() {
 
   return (
     <div className="results-page">
-      <img className="bubbles-animation" src={bubblesImg}></img>
+      <img className="bubbles-animation" src={bubblesBackground}/>
       <div className="results-content">
 
-          
+        <div className="winner-player-image-div">
+          <img className="winner-player-image" src={comAvatar}>
+          </img>
+        </div>
 
-          <div className="winner-player-image-div">
-            <img className="winner-player-image" src = {comAvatar}>
-            </img>
-          </div>
-
-          <img className="seaweed" src={seaweed}/>
+        <img className="seaweed" src={seaweed} />
 
         <div className="podium">
           <h1 className="winner-tag">Winner</h1>
           <h2 className="winner-player-tag">Player</h2>
-
         </div>
-        <div className="avatar1"></div>
-
       </div>
-
 
       <Link to="/" >
         <button className="next-btn"></button>
