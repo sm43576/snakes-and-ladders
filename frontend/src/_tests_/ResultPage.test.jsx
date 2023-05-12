06cfdea2b6ce8b89291e5f3dc0959b084f909dc5
rnat697 <rnat697@aucklanduni.ac.nz>
2023-05-12 15:55:18 +1200

    Fixed results page test

--- frontend/src/_tests_/ResultPage.test.jsx
+++ frontend/src/_tests_/ResultPage.test.jsx
@@ -11,17 +11,17 @@ test('AppContextProvider makes GET request to API to render player', ()=>{
     const   player = [{
         name:"Player 1",
         placement: 0,
-        image: "avatar_dolphin",
+        image: "avatar_dolphin.png",
         isHuman: true
       },
         {
             name:"Player 2",
             placement: 100,
-            image: "avatar_octopus",
+            image: "avatar_octopus.png",
             isHuman: true
         }];
       const initState = {
-          players: [player],
+          players: player,
           removePlayer: vi.fn()
     };
 
